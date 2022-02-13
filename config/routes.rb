f9de4dd79b137c8ebbd5a16610b2f356ee12a8e9
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2022-02-13 21:30:02 +0000

    basic working app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/create'
-  get 'posts/new'
-  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "pages#home"
 
-  resources :post, only: [:create, :new]
+  resources :posts, only: [:index, :show, :create, :new]
 end
